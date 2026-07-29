e989c43fec543cbe947d81b795207de9211cf7c6
dzelenke7 <dzelenke@gmail.com>
2026-07-28 22:59:54 -0400

    Add files via upload

---
+++ L-I-V-arduino.ino
@@ -0,0 +1,27 @@
+#define V0 0
+#define V1 1
+#define V2 2
+const float R_f = 1000.0;
+
+void setup() 
+{
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+  float V_0 = analogRead(V0) * (5.0 / 1023.0);
+  float V_1 = analogRead(V1) * (5.0 / 1023.0);
+  float V_amp = analogRead(V2) * (5.0 / 1023.0);
+
+  float I_res = (5 - V_0) / 100;
+  float V_led = V_0 - V_1;
+  float I_photo = - (V_amp / R_f);
+
+  Serial.println(I_res * 1000.0);
+  Serial.println(V_led);
+  Serial.println(I_photo * 1000.0);
+
+  delay(100);
+}
